865ad8c6cefdc4b1f6fd830114618fbaed08608c
= <=>
2022-01-25 23:37:27 +0700

    update product add fillter

---
+++ src/slide_range.js
@@ -0,0 +1,9 @@
+// import { IonRangeSliderEvent } from "ion-rangeslider";
+// import { IonRangeSliderOptions } from "ion-rangeslider";
+import { IonRangeSliderOptions } from "ion-rangeslider";
+$("#js-range-slider").ionRangeSlider({
+    min: 100,
+    max: 1000,
+    from: 550
+});
+  $("#js-range-slider").ionRangeSlider();
